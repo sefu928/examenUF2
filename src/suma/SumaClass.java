a6746449ad4a799c334be57dbfbf3f565d11bcdf
sefu928 <117985600+sefu928@users.noreply.github.com>
2023-05-12 10:35:07 +0200

    Update SumaClass.java

--- src/suma/SumaClass.java
+++ src/suma/SumaClass.java
@@ -2,14 +2,13 @@ package suma;
 
 public class SumaClass {
     
+    public int Sumar(int n1, int n2){
+        return n1 + n2;
+    }
     
-    
-    
-
-    
-    
-    
-    
+    public int getNum1(int num1){
+        return num1;
+        
     public int sumaValorAbsolut(int n1, int n2){
         
         return Math.abs(n1) + Math.abs(n2);
